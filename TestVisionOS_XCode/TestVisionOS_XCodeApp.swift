6ad7f5cc63888f3d49bd48b99ed5dc9469cbc329
Maëva Soulard <58474758+msoulard@users.noreply.github.com>
2024-03-18 10:42:55 +0100

    Initial Commit

---
+++ TestVisionOS_XCode/TestVisionOS_XCodeApp.swift
@@ -0,0 +1,21 @@
+//
+//  TestVisionOS_XCodeApp.swift
+//  TestVisionOS_XCode
+//
+//  Created by Maëva Soulard on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestVisionOS_XCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
